8f530859f3ec62f82fb911a88722b1d932a1a91a
asiddiqa249 <asiddiqa249@gmail.com>
2024-01-10 00:07:53 +0530

    new commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
 import './App.css';
 
 function App() {
+  const onToken = (token) => {
+    console.log(token);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <StripeCheckout
+        token={onToken}
+        name="Stripe Payment"
+        currency="inr"
+        amount={800}
+        stripeKey="pk_test_51OWf77SAeA3EdM5WJbmV4bvRukCEp5CcKSqLa04vPmaiTzzp8wDODTFDfTWiYigXMbwPCVOUTmGgNTgfnhwg8Tta00rq7biZdx"
+      />
     </div>
   );
 }
