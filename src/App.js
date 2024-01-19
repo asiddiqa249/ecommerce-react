a8257e27adcf33e90a3d46598b0f9f7ce22aa3f7
asiddiqa249 <asiddiqa249@gmail.com>
2024-01-19 15:18:27 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,19 +1,36 @@
-import React from 'react';
-import StripeCheckout from 'react-stripe-checkout';
-import './App.css';
+import React, { useState } from "react";
+import StripeCheckout from "react-stripe-checkout";
+import "./App.css";
 
 function App() {
+  const [amount, setAmount] = useState(0);
+
   const onToken = (token) => {
     console.log(token);
+    localStorage.setItem("data", JSON.stringify(token));
+  };
+
+  const handleAmountChange = (event) => {
+    const inputValue = event.target.value;
+    setAmount(parseInt(inputValue, 10) || 0);
   };
 
   return (
     <div className="App">
+      <div>
+        <label htmlFor="amountInput" >Enter Amount: </label>
+        <input
+          type="text"
+          id="amountInput"
+          value={amount}
+          onChange={handleAmountChange}
+        />
+      </div><br/>
       <StripeCheckout
         token={onToken}
         name="Stripe Payment"
         currency="inr"
-        amount={800}
+        amount={amount*100}
         stripeKey="pk_test_51OWf77SAeA3EdM5WJbmV4bvRukCEp5CcKSqLa04vPmaiTzzp8wDODTFDfTWiYigXMbwPCVOUTmGgNTgfnhwg8Tta00rq7biZdx"
       />
     </div>
